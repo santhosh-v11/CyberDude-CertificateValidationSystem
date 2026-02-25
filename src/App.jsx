a1fbd600d0dfa58da46dec0cae15e91c5edb0c3c
santhosh-v11 <santhosh592002@gmail.com>
2026-02-25 12:57:55 +0530

    feat:added footer ui

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Projects from './Components/Projects-section/Projects'
 import Navbar from "./Components/Navbar";
 import Hero from "./Pages/Hero";
+import Footer from './Components/Footer';
 
 
 function App() {
@@ -11,11 +12,12 @@ function App() {
 
   return (
     <>
-    <div className='min-h-screen bg-primary pb-10'>
+    <div className='min-h-screen bg-primary '>
             <Navbar />
             <Hero/>
             <Profile/>
             <Projects/>
+            <Footer/>
     </div>
     </>
   )
