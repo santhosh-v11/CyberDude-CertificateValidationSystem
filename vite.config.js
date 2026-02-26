39da586b5d64ab25dedb8856b6fc022aa78caabe
santhosh-v11 <santhosh592002@gmail.com>
2026-02-26 11:37:47 +0530

    fix: resolve merge conflicts in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-=======
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
->>>>>>> origin/pavithra-work
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-<<<<<<< HEAD
 })
-=======
-});
->>>>>>> origin/pavithra-work
